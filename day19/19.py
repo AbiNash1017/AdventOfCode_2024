5b63723aecb33a57f4756822f11acbcb53abc75f
Abinash Das <abinash9731@gmsil.com>
2024-12-20 22:56:00 +0530

    day19

---
+++ day19/19.py
@@ -0,0 +1,26 @@
+from functools import cache
+
+designs = []
+with open('D:/Advent of code/day19/input.txt', 'r') as f:
+    towels = f.readline().strip().split(', ')
+    f.readline()
+    for line in f.readlines():
+        designs.append(line.strip())
+
+@cache
+def num_arrangements(design, start=0):
+    if start >= len(design):
+        return 1
+    result = 0
+    for towel in towels:
+        if design[start: start+len(towel)] == towel:
+            result += num_arrangements(design, start+len(towel))
+    return result
+
+# part 1
+print('part 1:')
+print(sum(num_arrangements(design) > 0 for design in designs))
+
+# part 2
+print('part 2:')
+print(sum(map(num_arrangements, designs)))
